a20445ef32fa2435a3550988ae2015a73de197f3
User <abbymoraa876@gmail.com>
2025-10-27 10:26:16 +0300

    Edits updated

--- journee/vite.config.js
+++ journee/vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+const repoName = '/Journee/'
+
 export default defineConfig({
+  base: repoName,
   plugins: [react(), tailwindcss()],
 })
+
